ed5401e0236503704fdc1e11e8cf17848591f764
Alia Zat <67161403+ALiaZat@users.noreply.github.com>
2020-08-22 14:27:07 +0300

    Singleton Solution

---
+++ hw/src/Util/Logger.java
@@ -0,0 +1,25 @@
+package Util;
+
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Logger {
+    public static void log(String s) {
+        try {
+            FileWriter fw = new FileWriter("log_file.txt", true);
+            fw.write("New Log (" + getCurrentTime() + "): " + s + '\n');
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Done...\n");
+    }
+
+    private static String getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+        return sdf.format(cal.getTime());
+    }
+}
---
+++ hw/src/main.java
@@ -0,0 +1,19 @@
+import Forms.MainForm;
+
+import java.io.File;
+
+public class main {
+    public static void main(String[] arguments) {
+
+        // Delete log file on project start
+        File file = new File("log_file.txt");
+
+        if (file.delete()) {
+            System.out.println("File deleted successfully");
+        }
+
+        // Create main form
+        new MainForm();
+    }
+
+}
